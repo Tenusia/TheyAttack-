d0b2f47e0ddd8e5af2b3783ef46fb351a9d6e163
Tenusia <tenusia@gmail.com>
2023-01-16 14:13:56 +0900

    first commit. Add game sprites and flying functionality. Add moving background, particle effects. Add shooting system, damage and health counters, AI path movement.

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileLifetime = 5f;
+    [SerializeField] float baseFiringRate = 0.2f;
+
+    [Header("AI")]
+    [SerializeField] bool useAI;
+    [SerializeField] float fireRateVarience = 0f;
+    [SerializeField] float minimumFireRate = 0.2f;
+
+    [HideInInspector] public bool isFiring;
+
+    Coroutine firingCoroutine;
+
+    void Start()
+    {
+        if(useAI)
+        {
+            isFiring = true;
+        }
+    }
+
+    void Update()
+    {
+        Fire();
+    }
+
+    void Fire()
+    {
+        if(isFiring && firingCoroutine == null)
+        {
+            firingCoroutine = StartCoroutine(FireContinously());
+        }
+        else if(!isFiring && firingCoroutine != null)
+        {
+            StopCoroutine(firingCoroutine);
+            firingCoroutine = null;
+        }
+    }
+
+    IEnumerator FireContinously()
+    {
+        while(true)
+        {
+            GameObject instance = Instantiate(projectilePrefab, 
+                                            transform.position, 
+                                            Quaternion.identity);
+            Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
+            if(rb !=null)
+            {
+                rb.velocity = transform.up * projectileSpeed;
+            }
+
+            Destroy(instance, projectileLifetime);
+
+            float timeToNextProjectile = Random.Range(baseFiringRate - fireRateVarience, 
+                                                baseFiringRate + fireRateVarience);
+            Mathf.Clamp(timeToNextProjectile, minimumFireRate, float.MaxValue);
+
+            yield return new WaitForSeconds(timeToNextProjectile);
+        }
+    }
+}
